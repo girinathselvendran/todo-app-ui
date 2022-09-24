848e555a89583426873b4d53a6c196e70e567c84
Girinath Selvendran <89913730+girinathselvendran@users.noreply.github.com>
2022-09-24 19:24:20 +0530

    Update service.js

--- src/server/service.js
+++ src/server/service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API = "http://localhost:4000";
+// export const API = "http://localhost:4000";
+export const API = "https://todo-app-mern-api.herokuapp.com";
 // export const API = "https://giri-todo-node-api.herokuapp.com";
 // export const API = "https://todo-app-using-ionic.herokuapp.com";
 
